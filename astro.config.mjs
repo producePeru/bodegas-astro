47a032d92505cab698823747522da0982baa12f2
Elizabeth Holmes <eliza@pnte.com>
2026-08-11 16:54:01 -0500

    app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
+import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+
   integrations: [
     tailwind(),
     vue(),
